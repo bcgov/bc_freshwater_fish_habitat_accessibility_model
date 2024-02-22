707678bef3ecb9e2bd79baa327d1163bda2fc75b
Simon Norris <snorris@hillcrestgeo.ca>
2024-02-22 15:26:39 -0800

    use sozip to compress the output

--- freshwater_fish_habitat_accessibility_model.sh
+++ freshwater_fish_habitat_accessibility_model.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # Copyright 2024 Province of British Columbia
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,30 +14,17 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
-#!/bin/bash
 set -euxo pipefail
 
-# Dump freshwater_fish_habitat_accessibility_MODEL to file
-# https://bcfishpass.s3.us-west-2.amazonaws.com/freshwater_fish_habitat_accessibility_MODEL.gpkg.zip
-
-# create view
-psql $DATABASE_URL -f sql/freshwater_fish_habitat_accessibility_model.sql
-
-# clear any existing dumps
-rm -rf freshwater_fish_habitat_accessibility_MODEL.gpkg*
-
-echo 'dumping crossings'
 ogr2ogr \
     -f GPKG \
     freshwater_fish_habitat_accessibility_MODEL.gpkg \
     PG:$DATABASE_URL \
     -nln crossings \
     -nlt PointZM \
-    -sql "select * from bcfishpass.freshwater_fish_habitat_accessibility_model_crossings_vw"
+    -sql "select *
+          from bcfishpass.freshwater_fish_habitat_accessibility_model_crossings_vw"
 
-
-echo 'dumping barriers_salmon'
 ogr2ogr \
     -f GPKG \
     -append \
@@ -45,22 +34,20 @@ ogr2ogr \
     -nln barriers_salmon \
     -nlt PointZM \
     -sql "select 
-			 barriers_ch_cm_co_pk_sk_id,
-			 barrier_type,
-			 barrier_name,
-			 linear_feature_id,
-			 blue_line_key,
-			 watershed_key,
-			 downstream_route_measure,
-			 wscode_ltree as wscode,
-			 localcode_ltree as localcode,
-			 watershed_group_code,
-			 total_network_km,
-			 geom
-  		  from bcfishpass.barriers_ch_cm_co_pk_sk"
+            barriers_ch_cm_co_pk_sk_id,
+            barrier_type,
+            barrier_name,
+            linear_feature_id,
+            blue_line_key,
+            watershed_key,
+            downstream_route_measure,
+            wscode_ltree as wscode,
+            localcode_ltree as localcode,
+            watershed_group_code,
+            total_network_km,
+            geom
+            from bcfishpass.barriers_ch_cm_co_pk_sk"
 
-
-echo 'dumping barriers_steelhead'
 ogr2ogr \
     -f GPKG \
     -append \
@@ -84,7 +71,6 @@ ogr2ogr \
 			 geom
   		  from bcfishpass.barriers_st"
 
-echo 'dumping salmon/steelhead access model'
 ogr2ogr \
     -f GPKG \
     -append \
@@ -95,8 +81,6 @@ ogr2ogr \
     -nlt LineStringZM \
     -sql "select * from bcfishpass.freshwater_fish_habitat_accessibility_model_vw"
 
-
-echo 'dumping observations'
 ogr2ogr \
     -f GPKG \
     -append \
@@ -124,14 +108,13 @@ ogr2ogr \
      geom
      from bcfishpass.freshwater_fish_habitat_accessibility_model_observations_vw"   
 
-echo 'dump to MODEL.gpkg complete'    
+# requires gdal >=3.7
+sozip \
+  freshwater_fish_habitat_accessibility_MODEL.gpkg.zip \
+  freshwater_fish_habitat_accessibility_MODEL.gpkg
 
-
-# compress and publish to s3
-# note that 7zip is not included in the bcfishpass conda environment.yml file, use some other package manager to install
-7z a freshwater_fish_habitat_accessibility_MODEL.gpkg.zip freshwater_fish_habitat_accessibility_MODEL.gpkg
-aws s3 cp freshwater_fish_habitat_accessibility_MODEL.gpkg.zip s3://bcfishpass/
-aws s3api put-object-acl --bucket bcfishpass --key freshwater_fish_habitat_accessibility_MODEL.gpkg.zip --acl public-read
-
-# delete unzipped
-rm freshwater_fish_habitat_accessibility_MODEL.gpkg
+# bucket objects are not open by default
+aws s3api put-object-acl \
+  --bucket bcfishpass \
+  --key freshwater_fish_habitat_accessibility_MODEL.gpkg.zip \
+  --acl public-read
